78e75a8ae79f129df69499ede150ae62d06ba187
anggasulistiangga <anggasulistiangga@gmail.com>
2025-05-26 13:49:05 +0700

    update data

--- app/db_config.py
+++ app/db_config.py
@@ -1,16 +1,17 @@
 from sqlalchemy import create_engine
 import os
 
-DB_USER = os.getenv("DB_USER", "postgres")
+DB_USER = os.getenv("DB_USER", "postgres.trnvtnvfxrjzibactryb")
 DB_PASS = os.getenv("DB_PASS", "intermilan12")
 DB_NAME = os.getenv("DB_NAME", "postgres")
-DB_HOST = os.getenv("DB_HOST", "db.trnvtnvfxrjzibactryb.supabase.co")
+DB_HOST = os.getenv("DB_HOST", "aws-0-ap-southeast-1.pooler.supabase.com")
 DB_PORT = os.getenv("DB_PORT", "5432")
+DB_POOL_MODE = os.getenv("DB_POOL_MODE", "session")
 
 # Local
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Supabase
-# DATABASE_URL = f"postgresql://postgres:intermilan12@db.trnvtnvfxrjzibactryb.supabase.co:5432/postgres"
+# DATABASE_URL = f"postgresql://postgres.trnvtnvfxrjzibactryb:[YOUR-PASSWORD]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
