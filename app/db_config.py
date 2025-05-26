7bae54a7c3d1144da2b6dc5984e3130ef1b21e69
anggasulistiangga <anggasulistiangga@gmail.com>
2025-05-26 13:41:52 +0700

    update database

--- app/db_config.py
+++ app/db_config.py
@@ -3,14 +3,14 @@ import os
 
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASS = os.getenv("DB_PASS", "intermilan12")
-DB_NAME = os.getenv("DB_NAME", "analytics_db")
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+DB_HOST = os.getenv("DB_HOST", "db.trnvtnvfxrjzibactryb.supabase.co")
 DB_PORT = os.getenv("DB_PORT", "5432")
 
 # Local
-# DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Supabase
-DATABASE_URL = f"postgresql://postgres:intermilan12@db.trnvtnvfxrjzibactryb.supabase.co:5432/postgres"
+# DATABASE_URL = f"postgresql://postgres:intermilan12@db.trnvtnvfxrjzibactryb.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
