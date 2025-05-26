8c3f34735f46da4e02b040ed21793179b9d4f48c
anggasulistiangga <anggasulistiangga@gmail.com>
2025-05-26 13:52:20 +0700

    update d

--- app/db_config.py
+++ app/db_config.py
@@ -9,9 +9,9 @@ DB_PORT = os.getenv("DB_PORT", "5432")
 DB_POOL_MODE = os.getenv("DB_POOL_MODE", "session")
 
 # Local
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Supabase
-# DATABASE_URL = f"postgresql://postgres.trnvtnvfxrjzibactryb:[YOUR-PASSWORD]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
+DATABASE_URL = f"postgresql://postgres.trnvtnvfxrjzibactryb:intermilan12@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
